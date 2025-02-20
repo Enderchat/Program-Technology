630536109de7494c5c85bc21a0337feb224840cc
Enderchat <116150809+Enderchat@users.noreply.github.com>
2025-02-20 11:56:56 +0700

    Add files via upload

---
+++ Practik1/ValueObjects/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practik1.ValueObjects
+{
+    struct Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+
+        public string Note { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            Amount = amount;
+            Date = date;
+            Note = note;
+        }
+    }
+}
